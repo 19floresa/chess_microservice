9cc2c875a8ac18df08f898e445e68d4e260fd0f2
19floresa <alexander.flores0920@gmail.com>
2025-11-08 20:47:14 -0800

    added movement to queen and finished simple tests

--- src/queen.ts
+++ src/queen.ts
@@ -1,8 +1,29 @@
 import { Chesspiece } from "./chesspiece.js"
+import { Rook } from "./rook.js"
+import { Bishop } from "./bishop.js"
 
 export class Queen extends Chesspiece
 {
-    isPositionValid(xPos: number, yPos: number): boolean {
-        throw new Error("Method not implemented.");
+    checkRook(newX: number, newY: number): boolean
+    {
+        const [ x, y ] = this.getCurrentPosition()
+        const color = this.getColor()
+        const rook: Rook = new Rook(x, y, color)
+        return rook.isPositionValid(newX, newY)
+    }
+
+    checkBishop(newX: number, newY: number): boolean
+    {
+        const [ x, y ] = this.getCurrentPosition()
+        const color = this.getColor()
+        const bishop: Bishop = new Bishop(x, y, color)
+        return bishop.isPositionValid(newX, newY)
+    }
+
+    isPositionValid(newX: number, newY: number): boolean {
+        const isValidMoveRook: boolean = this.checkRook(newX, newY)
+        const isValidMoveBishop: boolean = this.checkBishop(newX, newY)
+        console.log(`Rook: ${isValidMoveRook}, Bishop: ${isValidMoveBishop}`)
+        return (isValidMoveRook || isValidMoveBishop)
     }
 }
