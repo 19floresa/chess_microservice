3f7236232f18e3aa4e7c53c8d0cf474eccd43b74
19floresa <alexander.flores0920@gmail.com>
2025-11-08 19:27:34 -0800

    rook movement built

--- main.ts
+++ main.ts
@@ -6,10 +6,18 @@ import { Queen } from "./src/queen.js"
 import { Rook } from "./src/rook.js"
 import { Color } from "./src/color.js"
 
-let newPiece: Pawn = new Pawn(7, 1, Color.black)
-console.log(newPiece.move(5, 1))
-console.log(newPiece.move(3, 1))
+const pawn: Pawn = new Pawn(7, 1, Color.black)
+console.log("Pawn-----")
+console.log(pawn.move(5, 1))
+console.log(pawn.move(3, 1))
 
 const king = new King(7, 1, Color.black)
+console.log("King-----")
 console.log(king.move(6, 1))
 console.log(king.move(3, 1))
+
+const rook: Rook = new Rook(7, 1, Color.black)
+console.log("Rook-----")
+console.log(rook.move(5, 1))
+console.log(rook.move(5, 0))
+console.log(rook.move(7, 7))
--- src/rook.ts
+++ src/rook.ts
@@ -4,12 +4,9 @@ export class Rook extends Chesspiece
 {
     isPositionValid(newX: number, newY: number): boolean
     {
-        return true
-    }
-
-    print(): void
-    {
-        const [ x, y ] = this.getCurrentPosition()
-        console.log(`${x}, ${y}`)
+        const [ xDif, yDif ] = this.calcPosDiffByGreater(newX, newY)
+        const isMovingVertically   = (xDif === 0) && (yDif !== 0)
+        const isMovingHorizontally = (xDif !== 0) && (yDif === 0)
+        return ( isMovingVertically ||isMovingHorizontally )
     }
 }
