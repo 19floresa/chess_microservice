dbc4509b10f25c73e9970bc42fb30dc7cbf417dd
19floresa <alexander.flores0920@gmail.com>
2025-11-08 20:04:34 -0800

    built knight movement

--- src/knight.ts
+++ src/knight.ts
@@ -2,7 +2,9 @@ import { Chesspiece } from "./chesspiece.js"
 
 export class Knight extends Chesspiece
 {
-    isPositionValid(xPos: number, yPos: number): boolean {
-        throw new Error("Method not implemented.");
+    isPositionValid(newX: number, newY: number): boolean 
+    {
+        const [ xDif, yDif ] = this.calcPosDiffByGreater(newX, newY)
+        return (xDif === yDif) && (xDif !== 0) && (yDif !== 0)
     }
 }
