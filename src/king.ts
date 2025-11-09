6b499b61983bafab034d37e55b481486d9583e5d
19floresa <alexander.flores0920@gmail.com>
2025-11-08 19:19:56 -0800

    implemented king movement

--- src/king.ts
+++ src/king.ts
@@ -2,7 +2,8 @@ import { Chesspiece } from "./chesspiece.js"
 
 export class King extends Chesspiece
 {
-    isPositionValid(xPos: number, yPos: number): boolean {
-        throw new Error("Method not implemented.");
+    isPositionValid(newX: number, newY: number): boolean {
+        const [ xDif, yDif ] = this.calcPosDiffByGreater(newX, newY)
+        return (xDif <= 1) && (yDif <= 1)
     }
 }
