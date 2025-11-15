82a0dd321efd4b2456ffe164f5fa4044d3a3fb70
19floresa <alexander.flores0920@gmail.com>
2025-11-14 20:59:34 -0800

    rook, queen, bishop dont jump over pieces

--- src/queen.ts
+++ src/queen.ts
@@ -4,19 +4,36 @@ import { Bishop } from "./bishop.js"
 
 export class Queen extends Chesspiece
 {
+    #bishop: Bishop
+    #rook: Rook
+    constructor(newX: number, newY: number, color: string)
+    {
+        super(newX, newY, color)
+        this.#bishop = new Bishop(newX, newY, color)
+        this.#rook = new Rook(newX, newY, color)
+    }
+
+    #getRook(): Rook
+    {
+        this.#rook.setNewPosition(...this.getCurrentPosition())
+        return this.#rook
+    }
+
+    #getBishop()
+    {
+        this.#bishop.setNewPosition(...this.getCurrentPosition())
+        return this.#bishop
+    }
+
     checkRook(newX: number, newY: number): boolean
     {
-        const [ x, y ] = this.getCurrentPosition()
-        const color = this.getColor()
-        const rook: Rook = new Rook(x, y, color)
+        const rook: Rook = this.#getRook()
         return rook.isPositionValid(newX, newY)
     }
 
     checkBishop(newX: number, newY: number): boolean
     {
-        const [ x, y ] = this.getCurrentPosition()
-        const color = this.getColor()
-        const bishop: Bishop = new Bishop(x, y, color)
+        const bishop: Bishop = this.#getBishop()
         return bishop.isPositionValid(newX, newY)
     }
 
@@ -24,5 +41,20 @@ export class Queen extends Chesspiece
         const isValidMoveRook: boolean = this.checkRook(newX, newY)
         const isValidMoveBishop: boolean = this.checkBishop(newX, newY)
         return (isValidMoveRook || isValidMoveBishop)
+    }
+    
+    checkJumpedSquares(gameBoard: Chesspiece[][], newX: number, newY: number): boolean
+    {
+        const [ xDif, yDif ] = this.calcPosDiff(newX, newY)
+        if ((xDif === 0) || (yDif === 0))
+        {
+            const rook: Rook = this.#getRook()
+            return rook.checkJumpedSquares(gameBoard, newX, newY)
+        }
+        else
+        {
+            const bishop: Bishop = this.#getBishop()
+            return bishop.checkJumpedSquares(gameBoard, newX, newY)
+        }
     }
 }
--- src/rook.ts
+++ src/rook.ts
@@ -9,4 +9,22 @@ export class Rook extends Chesspiece
         const isMovingHorizontally = (xDif !== 0) && (yDif === 0)
         return ( isMovingVertically ||isMovingHorizontally )
     }
+    
+    checkJumpedSquares(gameBoard: Chesspiece[][], newX: number, newY: number): boolean
+    {
+        const board: Chesspiece[][] = structuredClone(gameBoard)
+        const [ xDif, yDif ] = this.calcPosDiff(newX, newY)
+        const isNeg = (val: number) => val >= 0 ? 1 : -1 
+        const dif = ( xDif === 0 ) ? [ 0, isNeg(yDif) ] : [ isNeg(xDif), 0 ]
+        const totalDif: number = Math.max(...[ Math.abs(xDif), Math.abs(yDif) ])
+        for (let i = 1; i < totalDif; i++)
+        {
+            const [ hDif, vDif ] = dif
+            const newPosY: number = newY + (i * vDif)
+            const newPosX: number = newX + (i * hDif)
+            const piece: Chesspiece = board[newPosY][newPosX]
+            if (piece !== null) return false
+        }
+        return true
+    }
 }
