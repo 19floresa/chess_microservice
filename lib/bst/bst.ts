bb8a6925e9914451c111418910e864a710a9f852
19floresa <alexander.flores0920@gmail.com>
2025-12-18 08:50:20 -0800

    created func to add to bst; not working

--- lib/bst/bst.ts
+++ lib/bst/bst.ts
@@ -1,19 +1,19 @@
 
 
-class bst
+export class bst
 {
-    left: bst | null
-    right: bst | null
-    key: number
-    value: any
+    #left: bst | null = null
+    #right: bst | null = null
+    #key: number = -1
+    #value: any = null
 
     constructor(key: number, value: any, left: bst | null = null, 
                                          right: bst | null = null)
     {
-        this.left = null
-        this.right = null
-        this.key = key
-        this.value = value
+        this.#left = null
+        this.#right = null
+        this.#key = key
+        this.#value = value
     }
 
     #create(key: number, value: any, left: bst | null = null, 
@@ -27,48 +27,49 @@ class bst
         let current: bst | null = this
         while (current !== null)
         {
-            const currentKey = current.key
-            if (current === null || key == currentKey)
-            {
-                break
-            }
-            else if (key <= currentKey)
-            {
-                current = current.left
-            }
-            else
-            {
-                current = current.right
-            }
-        }
-        return current
-    }
-
-    #findParent(key: number): bst | null | undefined
-    {
-        let parent: bst | null = null
-        let current: bst | null = this
-        if (key === current.key)
-        {
-            return undefined
-        }
-
-        while (current !== null)
-        {
-            const currentKey = current.key
+            const currentKey = current.getKey()
             if (current === null || key === currentKey)
             {
                 break
             }
             else if (key <= currentKey)
             {
+                current = current.getLeft()
+            }
+            else
+            {
+                current = current.getRight()
+            }
+        }
+        return current
+    }
+
+    #findParent(key: number): bst
+    {
+        let parent: bst | null = null
+        let current: bst | null = this
+        if (key === current.getKey())
+        {
+            return current // Root case
+        }
+
+        while (current !== null)
+        {
+            const currentKey = current.getKey()
+            if (key === currentKey)
+            {
+                parent = null
+                break
+            }
+            else if (key <= currentKey)
+            {
                 parent = current
-                current = current.left
+                current = current.getLeft()
             }
             else
             {
                 parent = current
-                current = current.right
+                current = current.getRight()
             }
         }
         return parent
@@ -76,12 +77,69 @@ class bst
 
     add(key: number, value: any): boolean
     {
-        const nodeNew = this.#create(key, value)
+        const nodeParent = this.#findParent(key)
+        if (nodeParent !== null)
+        {
+            const nodeNew = this.#create(key, value)
+            const keyParent = nodeParent.getKey()
+            const keyNew = nodeNew.getKey()
+            if (keyNew <= keyParent)
+            {
+                nodeParent.setLeft(nodeNew)
+            }
+            else
+            {
+                nodeParent.setRight(nodeNew)
+            }
+            return true
+        }
         return false
     }
 
-    delete(): boolean
+    inorderTraversal(node: bst = this)
+    {
+        if (node !== null)
+        {
+            console.log(node.getKey())
+            this.inorderTraversal(node.getLeft())
+            this.inorderTraversal(node.getRight())
+        }
+    }
+
+    delete(key: number): boolean
     {
         return false
     }
+
+
+    getKey()
+    {
+        return this.#key
+    }
+
+    getValue()
+    {
+        return this.#value
+    }
+
+    getLeft()
+    {
+        return this.#left
+    }
+
+    getRight()
+    {
+        return this.#right
+    }
+
+    setLeft(node: bst)
+    {
+        this.#left = node
+    }
+
+    setRight(node: bst)
+    {
+        this.#right = node
+    }
 }
+
