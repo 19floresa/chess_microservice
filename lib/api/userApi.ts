1b8ca13b3119385fa5a4cc285b05c6e705c61122
19floresa <alexander.flores0920@gmail.com>
2026-01-01 22:04:52 -0800

    allows chrome to get cookies

--- lib/api/userApi.ts
+++ lib/api/userApi.ts
@@ -13,6 +13,7 @@ export async function login(username: string, password: string)
   return await fetch('http://localhost:3025/user/login', 
   { 
     method: 'POST', 
+    credentials: 'include',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   })
