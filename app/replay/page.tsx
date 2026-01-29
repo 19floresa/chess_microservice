48a7c528a1eef726dae82094d7452756168d0afd
19floresa <alexander.flores0920@gmail.com>
2026-01-28 21:37:58 -0800

    refactoring replay page with new components

--- app/replay/page.tsx
+++ app/replay/page.tsx
@@ -1,89 +1,41 @@
 "use client"
-import GameBoard from "@/components/GameBoard"
-import Chessboard from "@/engine/chessboard.ts"
 import { useState } from "react"
 
-import { positionProp } from "@/lib/types/props"
+import GameBoardMock from "@/components/GameBoardMock"
+import GameInfoBlock from "@/components/GameInfoBlock"
+import { gameStep } from "@/lib/types/gameSteps"
+
+const pageNumberMin: number = 1
+const pageNumberMax: number = 5
 
 export default function Page()
 {
-    const [ board, setBoard ] = useState(new Chessboard(true))
-    const [ prev, setPrev ] = useState([])
-    const [ next, setNext ] = useState([[ 1, 1, 1, 2 ], [ 1, 2, 1, 3 ], [ 1, 3, 1, 4 ] ])
+    const steps: gameStep[] = [ [ 1, 1, 1, 2, 0 ], [ 1, 2, 1, 3, 0 ], [ 1, 3, 1, 4, 0 ] ]
     const [ pageNumber, setPageNumber ] = useState(1)
-    const [ pageNumberMax, setPageNumberMax ] = useState(5)
-    const [ toggleButtons, setToggleButtons ] = useState([ false, true ])
-
-    const handleClick = ({ x, y }: positionProp) => { return }
-    const handleBoard = ({ x, y }: positionProp): string => board.getPieceName(x, y)
-
-    function onClickNext()
-    {
-        if (next.length === 0) return
-        const newNext = next.slice()
-        const newPrev = prev.slice()
-        const n = newNext.shift()
-        newPrev.push(n)
-        const [ x, y, x2, y2 ] = n
-        board.move({ x, y, x2, y2 })
-        setPrev(newPrev)
-        setNext(newNext)
-        setBoard(board)
-    }
-
-    function onClickPrev()
-    {
-        if (prev.length === 0) return
-        const newPrev = prev.slice()
-        const p = newPrev.pop()
-        const newNext = [p, ...next]
-        const [ x2, y2, x, y ] = p
-        board.move({ x, y, x2, y2 })
-        setPrev(newPrev)
-        setNext(newNext)
-        setBoard(board)
-    }
-
-    function onClickReset()
-    {
-        const newNext = [ ...prev, ...next ]
-        const newPrev = []
-        board.clearBoard()
-        board.init()
-        setPrev(newPrev)
-        setNext(newNext)
-        setBoard(board)
-    }
 
     async function onClickPagePrev()
     {
+        if (pageNumber === pageNumberMin) return
         const newPageNumber = pageNumber - 1
-        const newToggleButtons = (newPageNumber === 1)  ? [ false, true] : [ true, true ]
-        setToggleButtons(newToggleButtons)
         setPageNumber(newPageNumber)
     }
 
     async function onClickPageNext()
     {
+        if (pageNumber === pageNumberMax) return
         const newPageNumber = pageNumber + 1
-        const newToggleButtons = (newPageNumber === pageNumberMax)  ? [ true, false ] : [ true, true ]
-        setToggleButtons(newToggleButtons)
         setPageNumber(newPageNumber)
     }
 
     return (
     <>
-        <div>
-            <GameBoard handleClick={handleClick} handleBoard={handleBoard}/>
-            <button onClick={onClickPrev}>Previous Move</button>
-            <button onClick={onClickReset}>Reset Moves</button>
-            <button onClick={onClickNext}>Next Move</button>
-        </div>
-        <div>
-           { toggleButtons[0] ? <button onClick={onClickPagePrev}>Previous Games</button> : null }
+        <GameBoardMock steps={steps}/>
+        <GameInfoBlock/>
+        {/* <div>
+            <button onClick={onClickPagePrev}>Previous Games</button>
             {pageNumber}
-            { toggleButtons[1] ? <button onClick={onClickPageNext}>Next Games</button> : null }
-        </div>
+            <button onClick={onClickPageNext}>Next Games</button>
+        </div> */}
     </>
     )
 }
