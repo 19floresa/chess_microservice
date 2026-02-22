34b48f3c66860dbf18b647e4f44964a310902be5
19floresa <alexander.flores0920@gmail.com>
2026-02-22 14:14:29 -0800

    changed retrieve url to player because it was not supposed to be app

--- lib/api/userApi.ts
+++ lib/api/userApi.ts
@@ -1,6 +1,6 @@
 export async function register(username: string, password: string)
 {
-    return await fetch('http://app:3025/user/register', 
+    return await fetch('http://player:3025/user/register', 
     { 
       method: 'POST', 
       headers: { "Content-Type": "application/json" },
@@ -10,7 +10,7 @@ export async function register(username: string, password: string)
 
 export async function login(username: string, password: string)
 {
-  return await fetch('http://app:3025/user/login', 
+  return await fetch('http://player:3025/user/login', 
   { 
     method: 'POST', 
     credentials: 'include',
