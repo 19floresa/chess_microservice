1e8d5c491ae1b95dd94ab276c0969b249de8ce23
19floresa <alexander.flores0920@gmail.com>
2026-02-22 16:17:27 -0800

    removed credentials

--- lib/api/userApi.ts
+++ lib/api/userApi.ts
@@ -13,7 +13,6 @@ export async function login(username: string, password: string)
   return await fetch('http://localhost:3025/user/login', 
   { 
     method: 'POST', 
-    credentials: 'include',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   })
