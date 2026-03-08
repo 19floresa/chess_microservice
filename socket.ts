b0d78826bfaaa607dd14ab198778b5f116d987db
19floresa <alexander.flores0920@gmail.com>
2026-03-08 01:13:43 -0800

    changed socket url link

--- socket.ts
+++ socket.ts
@@ -1,8 +1,9 @@
 import { ClientToServerEvents, ServerToClientEvents } from "@/lib/types/socket.ts"
 import { io } from "socket.io-client"
 import type { Socket } from "socket.io-client"
+import config from "./lib/config/config";
 
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3056';
+const URL = process.env.NODE_ENV === 'production' ? undefined : `http://${config.URL}:3056`;
 
 export const socket: Socket = io(URL, { autoConnect: false }) // TODO: set to false, https://socket.io/how-to/use-with-react
